78332cddfabeb6b5e13cefbbe40a47e76e28306f
Aman Kumar <aman40895@gmail.com>
2022-06-04 17:19:31 +0530

    updated port

--- server/server.js
+++ server/server.js
@@ -17,6 +17,6 @@ app.get('/sortingvisualiser', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/sorting visualiser/index.html"));
 })
 app.get('/cool', (req, res) => res.send(cool()))
-app.listen(3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("listening on port 3000");
 });
